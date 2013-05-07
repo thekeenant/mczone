a8c400ea44df1f105b62852c24bc2c767fa12fb2
Haribo98 <harry@nebulaproject.net>
2013-05-07 15:58:01 +0100

    Matches...

--- Ghost/src/co/mczone/ghost/cmds/MatchesCmd.java
+++ Ghost/src/co/mczone/ghost/cmds/MatchesCmd.java
@@ -1,12 +1,24 @@
 package co.mczone.ghost.cmds;
 
+import java.util.List;
+
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
+import co.mczone.ghost.api.Match;
+
 public class MatchesCmd implements CommandExecutor {
+	
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		
+		List<Match> matches = Match.getList();
+		
+		for (Match match : matches) {
+			String t = match.getTitle();
+		}
+		
 		return false;
 	}
 
